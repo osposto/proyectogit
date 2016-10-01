fcc8b10276a15f7eb042c31f05a2ec828d8d0ecd
Orlando Sposto <osposto@adsservicios.com.ar>
2016-10-01 11:24:22 -0300

    Se agrega un mebsaje al sistema

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 /* Esto es el inicio del archivo en C
 *Vamos a ver cómo se desempeña todo eso de git...*/
+
+void fBienvenida()
+{
+	printf("Bienvenidos al nuevo mundo");
+}
 void main()
 {
 	printf("hola Mundo");
+	fBienvenida();
+	/*esto es una prueba de presentación*/
 }
